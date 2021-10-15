f3d23c151f80bde8e64116f35406eb18de023a27
107124 <kredfox4@gmail.com>
2021-10-15 10:19:37 -0400

    added a what's new section in the home pag

---
+++ src/components/whatsNew.js
@@ -0,0 +1,58 @@
+import React from "react";
+import YouTube from 'react-youtube';
+
+
+import "../styles/whatsNew.css"
+
+export default class WhatsNew extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+
+    render() {
+
+        const opts = {
+            height: '250',
+            width: '390',
+            playerVars: {
+                // https://developers.google.com/youtube/player_parameters
+                autoplay: 0,
+            },
+        };
+
+
+        return (
+            <div className="whats-new-container">
+                <div className="header">
+                    <p>What's new</p>
+                </div>
+                <div className="card-section">
+
+                    <div className="new-card">
+                        <YouTube
+                            videoId={"-X_QughltEI"}
+                            opts={opts}
+                        />
+                    </div>
+                    <div className="new-card">
+                        <YouTube
+                            videoId={"VQMjFLAIdYI"}
+                            opts={opts}
+                        />
+                    </div>
+                    <div className="new-card">
+                        <YouTube
+                            videoId={"uQTiEh4NGWY"}
+                            opts={opts}
+                        />
+                    </div>
+                </div>
+
+            </div>
+        );
+
+    }
+}
--- src/home.js
+++ src/home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./navigation";
 import Tilt from "react-parallax-tilt";
+import WhatsNew from "./components/whatsNew";
 
 import "./styles/home.css";
 
@@ -15,21 +16,30 @@ export default class Home extends React.Component {
     return (
       <div className="home-page">
         <NavBar />
-        <div>home</div>
         <div className="kent-header">
           <Tilt
             className="tilt"
             tiltEnable={true}
             scale={1}
-            perspective={500}
+            perspective={1000}
             glareEnable={true}
             transitionSpeed={1500}
             glareMaxOpacity={0.8}
             glareColor="#ffffff"
             glarePosition="bottom"
           >
-            <h1>KENT</h1>
+            <p>KENT</p>
           </Tilt>
+          <div className="subheading">
+            <h6>DEVELOPER</h6>
+            <h6>DESIGNER</h6>
+            <h6>YOUTUBER</h6>
+            <h6>TEACHER</h6>
+            <h6>MENTOR</h6>
+          </div>
+        </div>
+        <div className="whats-new">
+          <WhatsNew/>
         </div>
       </div>
     );

eb5cba43908e65e7b28057ff5707dea8dc484b77
107124 <kredfox4@gmail.com>
2021-10-21 11:38:49 -0400

    added props and project cards to recieve links and images

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,17 +3,25 @@ import React from "react";
 import "../styles/projects.css";
 
 export default class ProjectCard extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {};
-  }
+        this.state = {
+            header: props.header,
+            image: props.image,
+            link: props.link,
 
-  render() {
-    return (
-      <div className="project-card-page">
-        card
-      </div>
-    );
-  }
+        };
+    }
+
+    render() {
+        return (
+            <div className="project-card-page">
+                <div className="card">
+                    <img src={this.state.image} alt="image" />
+                    <a href={this.state.link}>{this.state.header}</a>
+                </div>
+            </div>
+        );
+    }
 }
--- src/projects.js
+++ src/projects.js
@@ -16,12 +16,14 @@ export default class Projects extends React.Component {
       <div className="projects-page">
         <NavBar />
         <div className="card-section">
-            <ProjectCard/>
-            <ProjectCard/>
-            <ProjectCard/>
-            <ProjectCard/>
-            <ProjectCard/>
-            <ProjectCard/>
+            <div className="cards">
+            <ProjectCard header="DIY Society" image="" link="https://github.com/107124/DIY"/>
+            <ProjectCard header="Bank" image="" link="https://scarn-credit-union-banking.herokuapp.com/"/>
+            <ProjectCard header="Family Chores" image="https://images.unsplash.com/photo-1533331034407-54ac4ace3562?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2148&q=80" link="https://family-chores-linked.netlify.com/"/>
+            <ProjectCard header="Health Journal" image="" link="https://health-journals.herokuapp.com/welcome"/>
+            <ProjectCard header="Dev Fries" image="" link="https://determined-sinoussi-b6e667.netlify.com/"/>
+            <ProjectCard header="Nevada Exhaust Cleaning" image="https://images.unsplash.com/photo-1591329207233-c3858778b45a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2370&q=80" link="https://nevada-exhaust-cleaning.netlify.com/"/>
+            </div>
         </div>
       </div>
     );
